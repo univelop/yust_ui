f1eeb1b2ba4ea9062783baffaa687408f0dda828
Jannis Rosenbaum <j.ro@dock51.de>
2022-10-28 11:49:31 +0200

    Change default for hideKeyboardOnAutofocus

--- lib/src/widgets/yust_number_field.dart
+++ lib/src/widgets/yust_number_field.dart
@@ -47,7 +47,7 @@ class YustNumberField extends StatelessWidget {
     this.suffixIcon,
     this.focusNode,
     this.autofocus = false,
-    this.hideKeyboardOnAutofocus = true,
+    this.hideKeyboardOnAutofocus = false,
     this.validator,
     this.divider = true,
   }) : super(key: key);
