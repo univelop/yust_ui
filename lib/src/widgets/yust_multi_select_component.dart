9c5a38f800beb18906dcef3a93c470a41bf9f57e
speydril <61617473+speydril@users.noreply.github.com>
2023-01-04 21:56:54 +0100

    add disabled and singleSelect parameter

--- lib/src/widgets/yust_multi_select_component.dart
+++ lib/src/widgets/yust_multi_select_component.dart
@@ -1,38 +1,59 @@
 import 'package:flutter/material.dart';
 
 import 'package:collection/collection.dart';
+import 'package:yust_ui/src/widgets/yust_list_tile.dart';
 
 class YustMultiSelectComponent<T> extends StatelessWidget {
   final List<T> optionValues;
   final List<String> optionLabels;
-  final List<T>? selectedValues;
-
-  const YustMultiSelectComponent({
+  final List<T> selectedValues;
+  final bool disabled;
+  final bool singleSelect;
+  final String noOptionsText;
+  YustMultiSelectComponent({
     Key? key,
     required this.optionValues,
     required this.optionLabels,
     List<T>? selectedValues,
-  })  : selectedValues = selectedValues ?? const [],
+    this.noOptionsText = 'Keine Optionen vorhanden',
+    this.disabled = false,
+    this.singleSelect = false,
+  })  : selectedValues = selectedValues ?? [],
         super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ...optionValues
-            .mapIndexed(
-              (int index, T optionValue) => StatefulBuilder(
-                builder: (_, StateSetter setState) => CheckboxListTile(
-                    title: Text(optionLabels[index]),
-                    value: selectedValues!.contains(optionValue),
-                    controlAffinity: ListTileControlAffinity.platform,
-                    onChanged: (value) => setState(() => (value ?? false)
-                        ? selectedValues!.add(optionValue)
-                        : selectedValues!.remove(optionValue))),
-              ),
-            )
-            .toList(),
-      ],
+    if (optionValues.isEmpty) {
+      return YustListTile(
+        label: noOptionsText,
+        center: true,
+      );
+    }
+    return StatefulBuilder(
+      builder: (_, StateSetter setState) => Column(
+        children: [
+          ...optionValues
+              .mapIndexed(
+                (int index, T optionValue) => CheckboxListTile(
+                  enabled: !isCheckBoxDisabled(optionValue),
+                  title: Text(optionLabels[index]),
+                  value: selectedValues.contains(optionValue),
+                  controlAffinity: ListTileControlAffinity.platform,
+                  onChanged: (value) => setState(() {
+                    (value ?? false)
+                        ? selectedValues.add(optionValue)
+                        : selectedValues.remove(optionValue);
+                  }),
+                ),
+              )
+              .toList(),
+        ],
+      ),
     );
   }
+
+  bool isCheckBoxDisabled(T value) =>
+      disabled ||
+      (singleSelect &&
+          (!selectedValues.contains(value) && selectedValues.isNotEmpty));
 }
