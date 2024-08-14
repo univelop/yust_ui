b82be2a5ad5d7417cb7bcaaf11ebaf443ea14fdd
Jannik <44322559+cJannik@users.noreply.github.com>
2024-08-14 15:57:45 +0200

    move dms formatting into yust

--- lib/src/services/yust_location_service.dart
+++ lib/src/services/yust_location_service.dart
@@ -149,37 +149,6 @@ class YustLocationService {
   /// returns [true].
   Future<bool> isGeocoderPresent() => isPresent();
 
-  String _formatCoordinateToDMS(double decimalDegree) {
-    int degrees = decimalDegree.truncate();
-    double decimalMinutes = (decimalDegree - degrees) * 60;
-    int minutes = decimalMinutes.truncate();
-    double seconds = (decimalMinutes - minutes) * 60;
-
-    return '$degrees° $minutes\' ${seconds.toStringAsFixed(2)}"';
-  }
-
-  /// Formats the given latitude to a string in DMS format
-  ///
-  /// Example: 48.858844 -> 48° 51' 31.84" N
-  String formatLatitudeToDMS(double latitude) {
-    String latitudeDirection = latitude >= 0 ? 'N' : 'S';
-
-    String latitudeDMS = _formatCoordinateToDMS(latitude.abs());
-
-    return '$latitudeDMS $latitudeDirection';
-  }
-
-  /// Formats the given longitude to a string in DMS format
-  ///
-  /// Example: 2.2943506 -> 2° 17' 39.67" E
-  String formatLongitudeToDMS(double longitude) {
-    String longitudeDirection = longitude >= 0 ? 'E' : 'W';
-
-    String longitudeDMS = _formatCoordinateToDMS(longitude.abs());
-
-    return '$longitudeDMS $longitudeDirection';
-  }
-
   /// Opens a stream of the current device position and shows a dialog to the user.
   /// The user can then choose to accept the current position, wait for a better accuracy or cancel the dialog.
   /// The [locale] parameter can be used to set the locale of the dialog
